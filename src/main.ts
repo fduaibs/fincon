3fa2f1d93ae404915323fe8441e083ddfc7f7c78
Felipe Duaibs <felipe.duaibs@hotmail.com>
2022-07-22 23:08:23 -0300

    feat: swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Fincon API')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(3000 || port);
